49afaaffd397f5bed3c61c3744615292bec41910
Alev-Narcin <alevnarcin@outlook.com>
2023-02-23 23:38:26 +0300

    145. Outputting Cart Items

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -23,9 +23,7 @@ const MealItemForm = (props) => {
         <Input
             ref={amountInputRef}
             label="Amount"
-            input={{
-                id: 'amount_' + props.id, type: 'number', min: '1', max: '5', step: '1', defaultValue: '1'
-            }}
+            input={{id: 'amount_' + props.id, type: 'number', min: '1', max: '5', step: '1', defaultValue: '1'}}
         />
         <button>+ Add</button>
         {!amountIsValid && <p>Please enter a valid amount (1-5).</p>}
